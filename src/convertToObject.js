72b13349eeddab0e96051f2bcc481c6cd9910869
chumachenkoUA <kirito22122005@gmail.com>
2025-08-30 00:21:00 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,10 +3,26 @@
 /**
  * @param {string} sourceString
  *
- * @return {object}
+ * @return {accect}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToaccect(sourceString) {
+  return sourceString
+    .split(';')
+    .map((line) => line.trim())
+    .filter((line) => line.length > 0)
+    .reduce((acc, line) => {
+      const [key, ...other] = line.split(':');
+
+      if (!key.trim() || other.length === 0) {
+        return acc;
+      }
+
+      const value = other.join(':');
+
+      acc[key.trim()] = value.trim();
+
+      return acc;
+    }, {});
 }
 
-module.exports = convertToObject;
+module.exports = convertToaccect;
